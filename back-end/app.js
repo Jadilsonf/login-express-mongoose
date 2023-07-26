f85bcaa34f6f82c613388117c9053dfb74bca9c7
Jadilson <80553013+Jadilsonf@users.noreply.github.com>
2023-07-26 12:56:41 -0300

    adcionado dotenv

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const app = express()
+const app = express();
+require("dotenv").config();
 
 app.use(cors())
 
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Servidor Online!")
 })
 
