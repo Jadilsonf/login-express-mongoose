881961521b6edbc00d5892eca1a13f23b22e6447
Jadilson <80553013+Jadilsonf@users.noreply.github.com>
2023-07-29 12:35:56 -0300

    Criação de usuários ativada

--- back-end/app.js
+++ back-end/app.js
@@ -5,24 +5,19 @@ const mongoose = require('mongoose')
 const userRouter = require('./routes/userRouter')
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_CONNECTION_URL, console.log('Mongo Connected'))
-    // {
-    //     useNewURLParser: true,
-    //     useUnifiedTopology: true
-    // },
-    // (error) => {
-    //     if (error)
-    //         console.log(error)
-    //         else
-    //         console.log('Mongo Connected')
-    // })
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  })
 
 app.use(cors())
 
 //O middleware abaixo está para  './user/login' ou './user/register
-app.use('/user', userRouter)
-
-app.use(express.json());
+app.use('/user', express.json(), userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Servidor Online!")
--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,8 +1,21 @@
+const User = require('../models/User')
+
 const userController = {
-    register: function (req,res) {
-        console.log('register')
-        res.send('<h1>Register</h1>')
+    register: async function (req,res) {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        try {
+            const savedUser = await user.save()
+            res.send(savedUser)
+        } catch(error){
+            res.status(400).send(error)
+        }
     },
+
+    
     login: function (req, res) {
         console.log('login')
         res.send('<h1>Login</h1>')
