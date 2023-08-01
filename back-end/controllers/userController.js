ca6243912806556aa7b674b4fe73894ddb4ca02b
Jadilson <80553013+Jadilsonf@users.noreply.github.com>
2023-08-01 17:09:13 -0300

    Bcrypt adcionado

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,11 +1,16 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 const userController = {
     register: async function (req,res) {
+
+        const selectedUser = await User.findOne({email:req.body.email})
+        if (selectedUser) return res.status(400).send('Email Already exists!')
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(req.body.password)
         })
         try {
             const savedUser = await user.save()
